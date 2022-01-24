9b1c19ac95010284629553efebcf470283403cb7
Hix <kuba.kubagalapagos@gmail.com>
2022-01-24 10:25:23 +0100

    www

---
+++ JS/komentarze.js
@@ -0,0 +1,13 @@
+/**
+ * @description returns sum of two elements
+ * @param {number} first component of sun
+ * @param {number} second component of sun
+ * @return {number} sum of two components
+ */
+//typ wartości w nawiasach, nazwa parametru, którego typ dotyczy, komentarz pod linią
+function sum(first, second)
+{
+    return first + second;
+}
+console.log(sum(3,2));
+alert(sum(3,2));
